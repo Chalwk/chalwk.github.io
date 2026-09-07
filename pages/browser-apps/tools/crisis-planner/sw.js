df6b2bc367ce72f787adfff3aabfd7a51ddae2c6
chalwk <chalwk.dev@gmail.com>
2026-09-08 10:47:02 +1200

    ...

--- pages/browser-apps/tools/crisis-planner/sw.js
+++ pages/browser-apps/tools/crisis-planner/sw.js
@@ -1,7 +1,7 @@
 ---
+layout: null
 ---
 // Copyright (c) 2024-2026 Jericho Crosby (Chalwk). All Rights Reserved.
 
 const CACHE_NAME = 'crisis-planner-{{ site.github.build_revision | default: site.time | date: "%s" }}';
-
 importScripts('/assets/js/sw-core.js?v={{ site.github.build_revision | default: site.time | date: "%s" }}');
