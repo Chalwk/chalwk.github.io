82daf09888e28098b3f68becf44e6d1c9549121b
Chalwk77 <jericho.crosby227@gmail.com>
2026-09-26 19:55:03 +1200

    ...

--- pages/browser-apps/tools/sensory-state-tracker/sw.js
+++ pages/browser-apps/tools/sensory-state-tracker/sw.js
@@ -3,6 +3,6 @@ layout: null
 ---
 // Copyright (c) 2024-2026 Jericho Crosby (Chalwk). All Rights Reserved.
 
-const CACHE_NAME = 'autism-symptom-tracker-{{ site.github.build_revision | default: site.time | date: "%s" }}';
+const CACHE_NAME = 'sensory-state-tracker-{{ site.github.build_revision | default: site.time | date: "%s" }}';
 
 importScripts('/assets/js/sw-core.js?v={{ site.github.build_revision | default: site.time | date: "%s" }}');
